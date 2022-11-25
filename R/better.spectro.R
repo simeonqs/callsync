7416ea8f9985857cbf03b4eb5d1103dc4d535d8c
Simeon Q. Smeele <simeonqs@hotmail.com>
2022-11-25 09:18:29 +0100

    Adding options to change cex to better.spectro. Still not working entirely, but not sure why. Function is working though, cex that is sometimes overwritten.

--- R/better.spectro.R
+++ R/better.spectro.R
@@ -9,6 +9,9 @@
 #' @param ylim numeric vector of length 2, limits for the y-axis. Default is no limits ('free').
 #' @param xlim numeric vector of length 2, limits for the x-axis. Default is no limits ('free').
 #' @param mar numeric vector of length 4, the margins of the plot for the `impagep` function.
+#' @param cex.main numeric the relative size of the title
+#' @param cex.lab numeric the relative size of the axis titles
+#' @param cex.axis numeric the relative size of the axis labels
 #' Default is `rep(3, 4)`.
 #'
 #' @return Plots the spectrogram to current window.
@@ -25,15 +28,18 @@ better.spectro = function(wave,
                           ovl = 450,
                           xlim = 'free',
                           ylim = 'free',
-                          mar = rep(3, 4)){
+                          mar = rep(3, 4),
+                          cex.main = 1,
+                          cex.axis = 0.75,
+                          cex.lab = 0.5){
 
   # Create spectrogram
   n = wl
   spec = signal::specgram(x = wave@left,
-                  n = n,
-                  Fs = wave@samp.rate,
-                  window = wl,
-                  overlap = ovl)
+                          n = n,
+                          Fs = wave@samp.rate,
+                          window = wl,
+                          overlap = ovl)
 
   # Discard phase information
   P = abs(spec$S)
@@ -53,18 +59,20 @@ better.spectro = function(wave,
 
   # Plot spectrogram
   oce::imagep(x = spec$t,
-         y = spec$f,
-         z = t(P),
-         ylab = 'Frequency [Hz]',
-         xlab = 'Time [s]',
-         zlim = c(min(P), max(P)),
-         xlim = xlim,
-         ylim = ylim,
-         col = hcl.colors(20, "RdBu", rev = TRUE) ,
-         drawPalette = F,
-         decimate = F,
-         main = main,
-         mar = mar,
-         cex.main = 0.5)
+              y = spec$f,
+              z = t(P),
+              ylab = 'Frequency [Hz]',
+              xlab = 'Time [s]',
+              zlim = c(min(P), max(P)),
+              xlim = xlim,
+              ylim = ylim,
+              col = hcl.colors(20, "RdBu", rev = TRUE) ,
+              drawPalette = F,
+              decimate = F,
+              main = main,
+              mar = mar,
+              cex.main = cex.main,
+              cex.axis = cex.axis,
+              cex.lab = cex.lab)
 
 }
