5aba91cf520853c8e2092bf796b3d0b7437988b9
Simeon Q. Smeele <simeonqs@hotmail.com>
2022-11-28 07:10:56 +0100

    Updating test_calc.perf. Wasn't really working before. Should be minimal test now.

--- tests/testthat/test_calc.perf.R
+++ tests/testthat/test_calc.perf.R
@@ -1,11 +1,9 @@
+d = load.selection.tables.audacity(path_selection_tables = 'files/audacity')
+
 test_that('Class.', {
 
-  d = load.selection.tables.audacity(path_selection_tables = 'files/audacity')
-  gt = load.selection.tables(path_selection_tables = 'files')
-
-  out = calc.perf(d, gt)
+  out = calc.perf(d, d)
 
   expect_equal(class(out), 'list')
 
 })
-
