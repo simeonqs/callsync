28bc98f528e84495327e0a6d8ffeed9e4ba7967b
Simeon Q. Smeele <simeonqs@hotmail.com>
2022-12-20 08:06:10 +0100

    Adding more tests.

--- tests/testthat/test_better.spectro.R
+++ tests/testthat/test_better.spectro.R
@@ -1,11 +1,28 @@
+wave = load.wave('files/wave_1.wav')
+tf = tempfile('.pdf')
+pdf(tf)
+
+
 test_that('Basics.', {
 
-  wave = load.wave('files/wave_1.wav')
   b = better.spectro(wave = wave, ylim = c(1000, 2000), xlim = c(0, 0.2))
 
   expect_true(all(b$yaxt >= 1000))
   expect_true(all(b$yaxt <= 2000))
   expect_true(all(b$xaxt <= 0.2))
 
+
 })
 
+test_that('Axis working.', {
+
+  b = better.spectro(wave = wave)
+
+  expect_true(all(b$xaxt <= 0))
+  expect_true(all(b$yaxt <= 0))
+
+})
+
+
+dev.off()
+unlink(tf)
