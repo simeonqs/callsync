6020ae022f16a5a3d98b89fadc5e953c337029e6
Simeon Q. Smeele <simeonqs@hotmail.com>
2023-02-15 15:52:01 +0100

    Minor edits to update.R.

--- update.R
+++ update.R
@@ -10,18 +10,16 @@ devtools::test_coverage()
 
 # Load
 library(callsync)
+citation('callsync')
 
 # Test more
 devtools::check()
+devtools::check_win_devel()
+devtools::spell_check()
 
 # CRAN
 results <- rhub::check_for_cran()
 results$cran_summary()
-usethis::use_cran_comments()
-
-devtools::check_win_devel()
-
-devtools::spell_check()
 
 # Good practice
 goodpractice::gp()
