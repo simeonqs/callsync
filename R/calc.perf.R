83d385b26ae85afa95e9b6529f80abf289f13dbf
au472091 <simeonqs@hotmail.com>
2023-09-11 16:00:40 +0200

    Updating calc.perf.R.
    Adding calculation of false negatives.
    Shortening the code to 80 characters.
    Using seq_len.

--- R/calc.perf.R
+++ R/calc.perf.R
@@ -1,14 +1,17 @@
 #' @title calc.perf
 #'
-#' @description Calculates the performance of the detections.
+#' @description Calculates the performance of the detections. Detections are
+#' true positive if they overlap to any extend with a ground truth selection.
 #'
-#' @param d data frame, detection selection table with start = start time in seconds, end = end time in
-#' seconds and file = file name
-#' @param gt data frame, ground truth selection table with start = start time in seconds, end = end time in
-#' seconds and file = file name
+#' @param d data frame, detection selection table with start = start time in
+#' seconds, end = end time in  seconds and file = file name
+#' @param gt data frame, ground truth selection table with start = start time
+#' in seconds, end = end time in seconds and file = file name
 #'
-#' @return Returns a named list with tp = the row numbers (in d) for the true positives, fp = the row numbers (in d) for the
-#' false positives, fp_rate = `length(pf)/nrow(d)` and tp_rate = `length(tp)/nrow(gt)`.
+#' @return Returns a named list with tp = the row numbers (in d) for the true
+#' positives, fp = the row numbers (in d) for the false positives, fn = the row
+#' numbers (in gt) for the false negatives, fp_rate = `length(pf)/nrow(d)` and
+#' tp_rate = `length(tp)/nrow(gt)`.
 #'
 #' @export
 
@@ -16,17 +19,43 @@ calc.perf = function(d, gt){
 
   # Find the tp and fp
   tp = fp = c()
-  for(i in 1:nrow(d)){
+  for(i in seq_len(nrow(d))){
     sub = gt[which(gt$file == d$file[i]),]
     if(nrow(sub) == 0) {fp = c(fp, i); next}
-    # keep if the start of the detection falls within a ground truth (after start and before end)
-    keep_start = sapply(1:nrow(sub), function(j) d$start[i] > sub$start[j] & d$start[i] < sub$end[j])
-    # or if the end falls within detections -> either case there is some overlap
-    keep_end = sapply(1:nrow(sub), function(j) d$end[i] > sub$start[j] & d$end[i] < sub$end[j])
+    # keep if the start of the detection falls within a ground truth (after
+    # start and before end)
+    keep_start = sapply(1:nrow(sub), function(j)
+      d$start[i] > sub$start[j] & d$start[i] < sub$end[j])
+    # or if the end falls within detections -> either case there is some
+    # overlap
+    keep_end = sapply(1:nrow(sub), function(j)
+      d$end[i] > sub$start[j] & d$end[i] < sub$end[j])
     # or if start and end fall around -> also overlap
-    keep_around = sapply(1:nrow(sub), function(j) d$start[i] < sub$start[j] & d$end[i] > sub$end[j])
+    keep_around = sapply(1:nrow(sub), function(j)
+      d$start[i] < sub$start[j] & d$end[i] > sub$end[j])
     keep = keep_start | keep_end | keep_around
     if(any(keep)) tp = c(tp, i) else fp = c(fp, i)
+  }
+
+  # Find the fn
+  fn = c()
+  for(i in seq_len(nrow(gt))){
+    # fn is no detections for that file
+    sub = d[which(d$file == gt$file[i]),]
+    if(nrow(sub) == 0) {fn = c(fn, i); next}
+    # keep if the start of the detection falls within a ground truth (after
+    # start and before end)
+    keep_start = sapply(1:nrow(sub), function(j)
+      gt$start[i] > sub$start[j] & gt$start[i] < sub$end[j])
+    # or if the end falls within detections -> either case there is some
+    # overlap
+    keep_end = sapply(1:nrow(sub), function(j)
+      gt$end[i] > sub$start[j] & gt$end[i] < sub$end[j])
+    # or if start and end fall around -> also overlap
+    keep_around = sapply(1:nrow(sub), function(j)
+      gt$start[i] < sub$start[j] & gt$end[i] > sub$end[j])
+    keep = keep_start | keep_end | keep_around
+    if(!any(keep)) fn = c(fn, i)
   }
 
   # Calculate rates
@@ -34,6 +63,10 @@ calc.perf = function(d, gt){
   tp_rate = length(tp)/nrow(gt)
 
   # Return
-  return(list(tp = tp, fp = fp, fp_rate = fp_rate, tp_rate = tp_rate))
+  return(list(tp = tp,
+              fp = fp,
+              fn = fn,
+              fp_rate = fp_rate,
+              tp_rate = tp_rate))
 
 }
