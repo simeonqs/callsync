afd1a92a2b399cf9eb61003a26a4e146422965b6
au472091 <simeonqs@hotmail.com>
2023-09-11 16:04:51 +0200

    Updating load.selection.tables.R. Moving some code into seperate function (load.selection.table.R).

--- R/load.selection.tables.R
+++ R/load.selection.tables.R
@@ -3,8 +3,8 @@
 #' @description Loads multiple Raven selection tables into one dataframe.
 #'  Also adds a column with file-selection
 #'
-#' @param path_selection_tables the path to the folder containing selection tables. Folder should not contain
-#' any other files.
+#' @param path_selection_tables the path to the folder containing selection
+#' tables. Folder should not contain any other files.
 #'
 #' @return Returns data frame with all selection tables.
 #'
@@ -15,25 +15,11 @@
 
 load.selection.tables = function(path_selection_tables){
 
-  selection_tables = path_selection_tables |>
-    list.files('*txt', full.names = TRUE) |>
-    lapply(function(x){
-      temp = read.csv(x, sep = '\t')
-      if(ncol(temp) != 8){ # if there are decibel columns, make sure it still works
-        temp = read.csv(x, sep = '\t',
-                        colClasses = c('numeric', 'character', 'numeric', 'numeric', 'numeric',
-                                       'numeric', 'numeric', 'character', 'character', 'character',
-                                       'character'))
-        temp = temp[,colnames(temp) %in% c('Selection', 'View', 'Channel', 'Begin.Time..s.', 'End.Time..s.',
-                                           'Low.Freq..Hz.', 'High.Freq..Hz.', 'Delta.Time..s.', 'Annotation')]
-      } else {
-        temp = read.csv(x, sep = '\t', colClasses = c('numeric', 'character', 'numeric', 'numeric', 'numeric',
-                                                      'numeric', 'numeric', 'character'))
-      }
-      return(temp)
-    })
-  names(selection_tables) = path_selection_tables |>
-    list.files('*txt') |> stringr::str_remove('.Table.1.selections.txt')
+  files = path_selection_tables |>
+    list.files('*txt', full.names = TRUE)
+  selection_tables = files |> lapply(load.selection.table)
+  names(selection_tables) = files |> basename()|>
+    stringr::str_remove('.Table.1.selections.txt')
   dat = selection_tables |> dplyr::bind_rows(.id = 'file') |> as.data.frame()
   dat = dat[dat$View == 'Waveform 1',]
 
