29e4c8f95efd73d8475e5c515797cc5fb1cd38c5
Simeon Q. Smeele <simeonqs@hotmail.com>
2023-10-03 15:16:04 +0200

    Updating calc.perf.R and calc.perf.Rd. Adding fn_rate explanation.

--- R/calc.perf.R
+++ R/calc.perf.R
@@ -10,8 +10,8 @@
 #'
 #' @return Returns a named list with tp = the row numbers (in d) for the true
 #' positives, fp = the row numbers (in d) for the false positives, fn = the row
-#' numbers (in gt) for the false negatives, fp_rate = `length(pf)/nrow(d)` and
-#' tp_rate = `length(tp)/nrow(gt)`.
+#' numbers (in gt) for the false negatives, fp_rate = `length(pf)/nrow(d)`,
+#' tp_rate = `length(tp)/nrow(gt)`, fn_rate = `length(fn)/nrow(gt)`.
 #'
 #' @examples
 #' require(callsync)
