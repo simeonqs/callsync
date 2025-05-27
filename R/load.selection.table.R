39d6ebef376debb3d592f3575078379edf93ee17
au472091 <simeonqs@hotmail.com>
2025-05-27 09:11:12 +0200

    Updating load.selection.table.R. Fixing variable types when loading selection table with 11 columns.

--- R/load.selection.table.R
+++ R/load.selection.table.R
@@ -32,16 +32,12 @@ load.selection.table = function(path_selection_table){
   selection_table = read.csv(path_selection_table, sep = '\t')
   # if there are decibel columns, make sure it still works
   if(ncol(selection_table) == 11){
-    temp = read.csv(path_selection_table, sep = '\t',
-                    colClasses = c('numeric', 'character', 'numeric',
-                                   'numeric', 'numeric', 'numeric', 'numeric',
-                                   'character', 'character', 'character',
-                                   'character'))
     selection_table =
-      selection_table[,colnames(selection_table) %in%
-                        c('Selection', 'View', 'Channel', 'Begin.Time..s.',
-                          'End.Time..s.', 'Low.Freq..Hz.', 'High.Freq..Hz.',
-                          'Delta.Time..s.', 'Annotation')]
+      read.csv(path_selection_table, sep = '\t',
+               colClasses = c('numeric', 'character', 'numeric',
+                              'numeric', 'numeric', 'numeric', 'numeric',
+                              'numeric', 'numeric', 'numeric',
+                              'character'))
   }
   if(ncol(selection_table) == 9){
     selection_table =
